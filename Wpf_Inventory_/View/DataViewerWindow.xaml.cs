bc004fa53bb885ccabfbc051dbf6452a63df6f76
Axemer <ulitracat@gmail.com>
2025-02-17 20:26:31 +0700

    refactoring

--- Wpf_Inventory_/View/DataViewerWindow.xaml.cs
+++ Wpf_Inventory_/View/DataViewerWindow.xaml.cs
@@ -27,46 +27,9 @@ namespace Wpf_Inventory_.View
 
         public void ShowData(object SelectedDevice, InventoryRegistryDataBaseEntities3 DBO)
         {
-            ////Device device = DBO.Device.ToList().FindIndex(GridIndex.);
-
-            //foreach (DeviceType deviceType in DBO.DeviceType.ToList())
-            //    DevTypeComboBox.Items.Add(deviceType.Type);
-
-            //foreach (Block block in DBO.Block.ToList())
-            //    DevBlockComboBox.Items.Add(block.Block1);
-
-            //foreach (Department department in DBO.Department.ToList())
-            //    DevDepComboBox.Items.Add(department.Name);
-
-            //foreach (Office office in DBO.Office.ToList())
-            //    DevOfficeComboBox.Items.Add(office.OfficeNum);
-
-
-            //// тут я начинаю из object выскивать сначала имена переменных потом уже сами переменные
-            //// Волшебная 6 
-            ////
-
-            //string[] aasd = SelectedDevice.GetType().GetProperties().Select(p => p.Name).ToArray();
-            ////DevIDTextBox.Text = aasd[6].ToString(); //GetType().GetProperty(Device_ID).GetValue(SelectedDevice, null).ToString();
-
-            //var obj = new List<object>();
-
-            //foreach (var prop in aasd)
-            //{
-            //    object PV = SelectedDevice.GetType().GetProperty(prop).GetValue(SelectedDevice, null);
-
-            //    obj.Add(PV);
-
-            //}
-
-            //// Вольшебная 6 это Device_ID потому что всегда в таком порядке раскладывается объект
-            //DevIDTextBox.Text = obj[6].ToString();
-
-            //var DevInf = SelectedDevice.GetType().GetProperty(DevIDTextBox.Text);
-
+           
             if (SelectedDevice == null || DBO == null) return;
 
-            
 
             // Получаем ID устройства из объекта SelectedDevice (если у него есть свойство Device_ID)
             PropertyInfo idProperty = SelectedDevice.GetType().GetProperty("Device_ID");
