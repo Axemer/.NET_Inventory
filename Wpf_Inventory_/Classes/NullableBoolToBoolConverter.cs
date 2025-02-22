0cf35497a4bc9aa5a0a313bd9780ab906912d618
Axemer <ulitracat@gmail.com>
2025-02-22 13:24:21 +0700

    - D9 - Пофиксил отображение чекбокса "списано"

---
+++ Wpf_Inventory_/Classes/NullableBoolToBoolConverter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace Wpf_Inventory_.Classes
+{
+    class NullableBoolToBoolConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value as bool? ?? false; // Если null, то false
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return (bool)value;
+        }
+    }
+}
