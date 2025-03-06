92f7afdaed6502b17ed31cefab53456c210c9718
Axemer <ulitracat@gmail.com>
2025-03-06 19:08:41 +0700

    - D17 - Сделал модель по базе на постгри на конец то добавил файл с коммандами для диспетчера пакетов Потерял очень много нерных клеток на ровном месте

---
+++ Wpf_Inventory_/Model/DeviceWorkplace.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+// Code scaffolded by EF Core assumes nullable reference types (NRTs) are not used or disabled.
+// If you have enabled NRTs for your project, then un-comment the following line:
+// #nullable disable
+
+namespace Wpf_Inventory_.Model
+{
+    public partial class DeviceWorkplace
+    {
+        public int DeviceWorkplaceId { get; set; }
+        public int WorkplaceId { get; set; }
+        public int DeviceId { get; set; }
+
+        public virtual Device Device { get; set; }
+        public virtual Workplace Workplace { get; set; }
+    }
+}
---
+++ Wpf_Inventory_/Model/Deviceparts.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+// Code scaffolded by EF Core assumes nullable reference types (NRTs) are not used or disabled.
+// If you have enabled NRTs for your project, then un-comment the following line:
+// #nullable disable
+
+namespace Wpf_Inventory_.Model
+{
+    public partial class Deviceparts
+    {
+        public Deviceparts()
+        {
+            DevicepartsDevice = new HashSet<DevicepartsDevice>();
+        }
+
+        public int DevicepartsId { get; set; }
+        public string Name { get; set; }
+
+        public virtual ICollection<DevicepartsDevice> DevicepartsDevice { get; set; }
+    }
+}
